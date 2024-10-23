fc2fcb62f78533b2b3376d55baf4c66a31954be3
Farooque Jamal <farooquejamalimran@gmail.com>
2024-10-23 14:56:42 +0530

    allowed hosts updated

--- ecom/settings.py
+++ ecom/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','farooque-ecommerce-django.up.railway.app','https://farooque-ecommerce-django.up.railway.app']
+ALLOWED_HOSTS = ['farooque-ecommerce-django.up.railway.app','https://farooque-ecommerce-django.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://farooque-ecommerce-django.up.railway.app']
 
 
